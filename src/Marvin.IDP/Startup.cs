6a3e100cd6fff89d4a2b1d2422bb2f0fe4d225ee
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-01-05 17:57:53 +0200

    Adding a User Interface for IdentityServer4

--- src/Marvin.IDP/Startup.cs
+++ src/Marvin.IDP/Startup.cs
@@ -16,6 +16,8 @@ namespace Marvin.IDP
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
                 .AddTestUsers(Config.GetUsers())
@@ -35,6 +37,10 @@ namespace Marvin.IDP
             }
 
             app.UseIdentityServer();
+
+            app.UseStaticFiles();
+
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
